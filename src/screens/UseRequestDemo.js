0d8b14026ceeb28bebd4c5bd2a08aec49690dc07
Ben <ben@aiztechlabs.com>
2026-01-28 21:21:52 -0500

    Initial import

---
+++ src/screens/UseRequestDemo.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function UseRequestDemo() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Interview Sandbox Ready</Text>
+    </View>
+  );
+}
